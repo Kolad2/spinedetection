2bbd25dfc030e148057340d2d74a66c0bd4cbdc9
koladik <koladik@gmail.com>
2026-07-22 22:58:20 +0300

    rvm added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spinedetection
+
+rvm [checkpoint](https://disk.yandex.ru/d/Ggj4-coVDXQiXQ) need place to "rvm/modelcheckpoint"
--- rvm/main.py
+++ rvm/main.py
@@ -11,6 +11,32 @@ def main():
     run_rvm(image_path, checkpoint_path)
 
 
+class HumanMatting:
+    def __init__(self, checkpoint_path: str, device=None):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = MattingNetwork("resnet50").to(self.device).eval()
+        self.model.load_state_dict(torch.load(checkpoint_path, map_location=device))
+
+    def __call__(self, image: np.ndarray):
+        image_tensor = (
+            torch.from_numpy(image)
+            .permute(2, 0, 1)
+            .float()
+            .div(255.0)
+            .unsqueeze(0)
+            .to(self.device)
+        )
+
+        # Начальные скрытые состояния (для одиночного изображения)
+        rec = [None] * 4
+
+        with torch.no_grad():
+            fgr, pha, *rec = self.model(image_tensor, *rec)
+
+        mask = pha[0, 0].cpu().numpy()
+        mask = (mask > 0.5).astype(np.uint8) * 255
+        return mask
+
 def run_rvm(image_path: str, checkpoint_path: str, backbone: str = "resnet50"):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
