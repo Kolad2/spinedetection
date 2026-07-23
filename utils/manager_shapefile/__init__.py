4e75c95dd7ea0f0c7843780375882eab88852524
koladik <koladik@gmail.com>
2026-07-23 20:21:08 +0300

    label update

--- utils/manager_shapefile/__init__.py
+++ utils/manager_shapefile/__init__.py
@@ -1,1 +1,3 @@
 from .mask_save import mask_save
+from .mask_load import mask_load
+from .mask_load import label_load
--- utils/manager_shapefile/mask_load.py
+++ utils/manager_shapefile/mask_load.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+from .shape_load import shape_load
 
 def mask_load(
     path: Path,
@@ -17,7 +18,14 @@ def mask_load(
         raise ValueError(
             f"Ожидался POLYGON, получен {shape_type}"
         )
+    mask = mask_prepare(lines, shape)
+    return mask
 
+
+def mask_prepare(
+    lines: list[np.ndarray],
+    shape: tuple[int, ...]
+) -> np.ndarray:
     height, width = shape[:2]
 
     mask = np.zeros(
@@ -25,11 +33,64 @@ def mask_load(
         dtype=np.uint8,
     )
 
-    if lines:
-        cv2.fillPoly(
-            mask,
-            lines,
-            255,
-        )
+    cv2.fillPoly(
+        mask,
+        lines,
+        255,
+    )
 
     return mask
+
+
+def label_load(
+    path: Path,
+    shape: tuple[int, ...],
+    thickness: int = 1,
+) -> np.ndarray:
+    lines, shape_type = shape_load(path)
+
+    if shape_type not in {
+        "POLYLINE",
+        "POLYLINEZ",
+        "POLYLINEM",
+    }:
+        raise ValueError(
+            f"Ожидался POLYLINE, получен {shape_type}"
+        )
+
+    label = label_prepare(
+        lines=lines,
+        shape=shape,
+        thickness=thickness,
+    )
+
+    return label
+
+
+def label_prepare(
+    lines: list[np.ndarray],
+    shape: tuple[int, ...],
+    thickness: int = 1,
+) -> np.ndarray:
+    if thickness <= 0:
+        raise ValueError(
+            f"Толщина линии должна быть положительной: {thickness}"
+        )
+
+    height, width = shape[:2]
+
+    label = np.zeros(
+        (height, width),
+        dtype=np.uint8,
+    )
+
+    cv2.polylines(
+        label,
+        lines,
+        isClosed=False,
+        color=255,
+        thickness=thickness,
+        lineType=cv2.LINE_8,
+    )
+
+    return label
